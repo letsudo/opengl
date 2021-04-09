8f8e27e867c053ba77e74aef197c31f5b8b16c96
yaoxiang <yaoxiang@kuaishou.com>
2021-04-09 09:39:22 +0800

    材质-光的属性

--- main.cpp
+++ main.cpp
@@ -189,6 +189,10 @@ int main()
         lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
         lightingShader.setFloat("material.shininess", 32.0f);
 
+        lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
+        lightingShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // 将光照调暗了一些以搭配场景
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f); 
+
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
--- shader/light_shader.fs
+++ shader/light_shader.fs
@@ -8,6 +8,15 @@ struct Material {
     float shininess;
 }; 
 uniform Material material;
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform Light light;
   
 uniform vec3 objectColor;
 uniform vec3 lightColor;
@@ -18,19 +27,19 @@ in vec3 FragPos;
 void main()
 {
      // 环境光
-    vec3 ambient = lightColor * material.ambient;
+    vec3 ambient = light.ambient * material.ambient;
 
     // 漫反射 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = lightColor * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
     // 镜面光
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = lightColor * (spec * material.specular);  
+    vec3 specular = light.specular  * (spec * material.specular);  
 
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
